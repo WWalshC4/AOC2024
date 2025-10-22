ffc2780884db0fb38da9ba605f1a9d9d9f467775
William Walsh <wwalsh@control4.com>
2025-10-22 21:46:44 +0100

    solve day1 for real input

--- python/day1/day1a.py
+++ python/day1/day1a.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python3
 
+import os
+
+inputFile = open ('./python/day1/input.txt', 'r')
+data = inputFile.read ()
+inputFile.close ()
+
 test = '''3   4
 4   3
 2   5
@@ -7,17 +13,17 @@ test = '''3   4
 3   9
 3   3'''
 #totalDistance == 11
+#data = test
 
 left = []
 right = []
 
-data = test
-
 lines = data.split ('\n')
 for line in lines:
-	locationIds = line.split ()
-	left.append (locationIds [0])
-	right.append (locationIds [1])
+	if (len (line) > 0):
+		locationIds = line.split ()
+		left.append (locationIds [0])
+		right.append (locationIds [1])
 
 left.sort ()
 right.sort ()
@@ -25,7 +31,7 @@ right.sort ()
 totalDistance = 0
 for i, leftElement in enumerate (left):
 	rightElement = right [i]
-	difference = int (rightElement) - int (leftElement)
+	difference = abs (int (rightElement) - int (leftElement))
 	totalDistance += difference
 
 print (totalDistance)
