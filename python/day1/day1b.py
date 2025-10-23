cd8ecd20080c686207c73791df934e68df68775c
William Walsh <wwalsh@control4.com>
2025-10-23 07:19:43 +0100

    Day 1 B complete

---
+++ python/day1/day1b.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+
+import os
+
+inputFile = open ('./python/day1/input.txt', 'r')
+data = inputFile.read ()
+inputFile.close ()
+
+test = '''3   4
+4   3
+2   5
+1   3
+3   9
+3   3'''
+#totalSimilarity == 31
+#data = test
+
+left = []
+right = []
+
+lines = data.split ('\n')
+for line in lines:
+	if (len (line) > 0):
+		locationIds = line.split ()
+		left.append (locationIds [0])
+		right.append (locationIds [1])
+
+rightCount = {}
+
+for num in right:
+	if num in rightCount:
+		rightCount [num] += 1
+	else:
+		rightCount [num] = 1
+
+
+totalSimilarity = 0
+for i, leftElement in enumerate (left):
+	similarity = 0
+	if leftElement in rightCount:
+		similarity = int (leftElement) * rightCount [leftElement]
+	totalSimilarity += similarity
+
+print (totalSimilarity)
