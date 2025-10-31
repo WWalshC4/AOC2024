9867470546a8d0e94002cdd2b3bd6a3390a29ac5
William Walsh <wwalsh@control4.com>
2025-10-31 08:16:44 +0000

    day3a solved

---
+++ python/day3/day3a.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+import os
+import re
+import math
+
+inputFile = open ('./python/day3/input.txt', 'r')
+data = inputFile.read ()
+inputFile.close ()
+
+test = '''xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))'''
+#total == 161
+#data = test
+
+lines = data.splitlines ()
+
+total = 0
+
+for line in lines:
+	validMuls = re.findall (r'mul\((\d+)\,(\d+)\)', line)
+	for validMul in validMuls:
+		ints = list (map (int, validMul))
+		result = math.prod (ints)
+		total += result
+
+print (total)
