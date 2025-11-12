728af5aedb6a37ac25064710e902e1633ee3a426
William Walsh <wwalsh@control4.com>
2025-11-12 12:35:54 +0000

    day4b complete

---
+++ python/day4/day4b.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python3
+
+import os
+
+inputFile = open ('./python/day4/input.txt', 'r')
+data = inputFile.read ()
+inputFile.close ()
+
+test = '''MMMSXXMASM
+MSAMXMSMSA
+AMXSXMAAMM
+MSAMASMSMX
+XMASAMXAMM
+XXAMMXXAMA
+SMSMSASXSS
+SAXAMASAAA
+MAMMMXMMMM
+MXMXAXMASX'''
+#totalCount == 9
+#data = test
+
+totalCount = 0
+
+grid = []
+
+lines = data.splitlines ()
+
+for line in lines:
+	gridline = list (line)
+	grid.append (gridline)
+
+def checkDirection (xPos, yPos):
+	if (xPos < 1):
+		return False
+	if (yPos < 1):
+		return False
+	if (yPos + 1 >= len (grid)):
+		return False
+	row = grid [yPos]
+	if (xPos + 1 >= len (row)):
+		return False
+
+	tl = grid [yPos - 1][xPos - 1]
+	tr = grid [yPos - 1][xPos + 1]
+	bl = grid [yPos + 1][xPos - 1]
+	br = grid [yPos + 1][xPos + 1]
+
+	if (tl == 'M'):
+		if (br == 'S'):
+			if (tr == 'M'):
+				if (bl == 'S'):
+					return True
+				else:
+					return False
+			elif (tr == 'S'):
+				if (bl == 'M'):
+					return True
+				else:
+					return False
+			else:
+				return False
+		else:
+			return False
+	elif (tl == 'S'):
+		if (br == 'M'):
+			if (tr == 'M'):
+				if (bl == 'S'):
+					return True
+				else:
+					return False
+			elif (tr == 'S'):
+				if (bl == 'M'):
+					return True
+				else:
+					return False
+			else:
+				return False
+		else:
+			return False
+	else:
+		return False
+
+for yPos, row in enumerate (grid):
+	for xPos, letter in enumerate (row):
+		if (letter == 'A'):
+			xmas = checkDirection (xPos, yPos)
+			if (xmas):
+				totalCount += 1
+
+print (totalCount)
