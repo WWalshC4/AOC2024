54ec250090852df383c98096e1f34ae48d1e3b2e
William Walsh <wwalsh@control4.com>
2025-11-15 08:38:02 +0000

    day5b complete

--- python/day5/day5b.py
+++ python/day5/day5b.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python3
 
+from functools import cmp_to_key
+
 import os
 
-inputFile = open ('./python/day5/input.txt', 'r')
-data = inputFile.read ()
-inputFile.close ()
+inputFile = open("./python/day5/input.txt", "r")
+data = inputFile.read()
+inputFile.close()
 
-test = '''47|53
+test = """47|53
 97|13
 97|61
 97|47
@@ -33,56 +35,90 @@ test = '''47|53
 75,29,13
 75,97,47,61,53
 61,13,29
-97,13,75,29,47'''
-#totalOfCorrectMiddlePages == 143
-#totalOfFixedMiddlePages == 123
-data = test
+97,13,75,29,47"""
+# totalOfCorrectMiddlePages == 143#
+# totalOfFixedMiddlePages == 123
+# data = test
 
 totalOfCorrectMiddlePages = 0
 totalOfFixedMiddlePages = 0
 
-sections = data.split ('\n\n')
+sections = data.split("\n\n")
 
-rulesRaw = sections [0].splitlines ()
-printRunsRaw = sections [1].splitlines ()
+rulesRaw = sections[0].splitlines()
+printRunsRaw = sections[1].splitlines()
 
 rulesByBefore = {}
 rulesByAfter = {}
 
+pageNumbersMap = {}
+
 for rule in rulesRaw:
-	ruleData = list (map (int, rule.split ('|')))
-	before = ruleData [0]
-	after = ruleData [1]
-	if not before in rulesByBefore:
-		rulesByBefore [before] = {}
-	if not after in rulesByAfter:
-		rulesByAfter [after] = {}
-	rulesByBefore [before] [after] = True
-	rulesByAfter [after] [before] = True
+    ruleData = list(map(int, rule.split("|")))
+    before = ruleData[0]
+    after = ruleData[1]
+    if not before in rulesByBefore:
+        rulesByBefore[before] = {}
+    if not after in rulesByAfter:
+        rulesByAfter[after] = {}
+
+    if not before in pageNumbersMap:
+        pageNumbersMap[before] = True
+    if not after in pageNumbersMap:
+        pageNumbersMap[after] = True
+    rulesByBefore[before][after] = True
+    rulesByAfter[after][before] = True
+
+
+def compare(page1, page2):
+    value = 0
+    # 	print (str(page1) + ':' + str(page2))
+    if page1 in rulesByBefore and page2 in rulesByBefore[page1]:
+        if not (value == 0 or value == -1):
+            print("conflict rule a:" + str(page1) + ":" + str(page2))
+        value = -1
+    elif page2 in rulesByAfter and page1 in rulesByAfter[page2]:
+        if not (value == 0 or value == -1):
+            print("conflict rule b:" + str(page1) + ":" + str(page2))
+        value = -1
+    elif page2 in rulesByBefore and page1 in rulesByBefore[page2]:
+        if not (value == 0 or value == 1):
+            print("conflict rule c:" + str(page1) + ":" + str(page2))
+        value = 1
+    elif page1 in rulesByAfter and page2 in rulesByAfter[page1]:
+        if not (value == 0 or value == 1):
+            print("conflict rule d:" + str(page1) + ":" + str(page2))
+        value = 1
+    else:
+        print("unknown rule: " + str(page1) + ":" + str(page2))
+        value = 0
+    return value
+
+
+pageNumberOrder = sorted(list(pageNumbersMap), key=cmp_to_key(compare))
 
 printRuns = []
 
 for printRunRaw in printRunsRaw:
-	printRun = list (map (int, printRunRaw.split (',')))
-	printRuns.append (printRun)
+    printRun = list(map(int, printRunRaw.split(",")))
+    printRuns.append(printRun)
 
 printRunsToBeFixed = []
 
 for printRun in printRuns:
-	valid = True
-	pagesSeen = {}
-	for page in printRun:
-		for seenPage in pagesSeen:
-			if (page in rulesByBefore and seenPage in rulesByBefore [page]):
-				valid = False
-		pagesSeen [page] = True
-	if (valid):
-		middlePos = int(len (printRun) / 2) #0 indexed!
-		middlePage = printRun [middlePos]
-		totalOfCorrectMiddlePages += middlePage
-	else:
-		printRunsToBeFixed.append (printRun)
+    sortedPrintRun = sorted(printRun, key=cmp_to_key(compare))
+    if printRun == sortedPrintRun:
+        middlePos = int(len(printRun) / 2)  # 0 indexed!
+        middlePage = printRun[middlePos]
+        totalOfCorrectMiddlePages += middlePage
+    else:
+        printRunsToBeFixed.append(printRun)
 
-print (printRunsToBeFixed)
+for printRun in printRunsToBeFixed:
+    fixedPrintRun = sorted(printRun, key=cmp_to_key(compare))
+    middlePos = int(len(fixedPrintRun) / 2)  # 0 indexed!
+    middlePage = fixedPrintRun[middlePos]
+    totalOfFixedMiddlePages += middlePage
 
-print (totalOfFixedMiddlePages)
+print(totalOfCorrectMiddlePages)
+print(totalOfFixedMiddlePages)
