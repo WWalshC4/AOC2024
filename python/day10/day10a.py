a48216e0f578d9b20e84c883c5692652f85287ca
William Walsh <wwalsh@control4.com>
2025-11-22 08:36:52 +0000

    day10a complete

--- python/day10/day10a.py
+++ python/day10/day10a.py
@@ -2,73 +2,35 @@
 
 import os
 
-inputFile = open("./python/day10/input.txt", "r")
+inputFile = open("./input.txt", "r")
 data = inputFile.read()
 inputFile.close()
 
-test = """89010123
-78121874
-87430965
-96549874
-45678903
-32019012
-01329801
-10456732"""
-# totalScore == 36
+test = """125 17"""
+# stoneCount25 == 55312
 # data = test
 
-grid = []
 
-trailheads = []
-
-lines = data.splitlines()
-
-for line in lines:
-    gridline = []
-    for letter in line:
-        try:
-            value = int(letter)
-        except:
-            value = -1
-        gridline.append(value)
-    grid.append(gridline)
-
-for yPos, row in enumerate(grid):
-    for xPos, height in enumerate(row):
-        if height == 0:
-            trailheads.append([xPos, yPos])
-
-maxY = len(grid)
-maxX = len(grid[0])
+def blink(stones):
+    newstones = []
+    for stone in stones:
+        stoneString = str(stone)
+        if stone == 0:
+            newstones.append(1)
+        elif len(stoneString) % 2 == 0:
+            half = int(len(stoneString) / 2)
+            newstones.append(int(stoneString[:half]))
+            newstones.append(int(stoneString[half:]))
+        else:
+            newstones.append(stone * 2024)
+    return newstones
 
 
-def climb(xPos, yPos, peaks):
-    currentHeight = grid[yPos][xPos]
-    location = [xPos, yPos]
-    if currentHeight == 9:
-        if not location in peaks:
-            peaks.append(location)
-        return
-    for xDiff in range(-1, 2):
-        for yDiff in range(-1, 2):
-            if abs(xDiff) == abs(yDiff):
-                continue
-            newX = xPos + xDiff
-            newY = yPos + yDiff
-            if 0 <= newX < maxX and 0 <= newY < maxY:
-                nextHeight = grid[newY][newX]
-                nextLocation = [newX, newY]
-                if nextHeight == currentHeight + 1:
-                    climb(newX, newY, peaks)
+stones = list(map(int, data.split()))
 
+for index in range(25):
+    stones = blink(stones)
+    # print(stones)
 
-totalScore = 0
-
-for trailhead in trailheads:
-    xPos = trailhead[0]
-    yPos = trailhead[1]
-    peaks = []
-    climb(xPos, yPos, peaks)
-    totalScore += len(peaks)
-
-print(totalScore)
+stoneCount25 = len(stones)
+print(stoneCount25)
